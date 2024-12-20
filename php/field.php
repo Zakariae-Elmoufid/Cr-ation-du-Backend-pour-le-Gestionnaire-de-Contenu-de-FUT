3db50179467a95e478ccf84187ab6662cdd5bdca
zakariae elmoufid <moufidzakarie3@gmail.com>
2024-12-20 15:07:38 +0100

    update

---
+++ php/field.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<div class="play  container">
+            <div class="field">
+
+                <div class="attack ">
+                    <div class="cart lw"  id="LW">
+                        <div class="event" ></div>
+                    </div>
+                    <div class="cart st " id="ST">
+                        <div class="event" ></div>
+                    </div>
+                    <div class="cart rw " id="RW">
+                        <div class="event" ></div>
+                    </div>
+                </div>
+                
+                <div class="midfield ">
+                    <div class="cart cdm " id="CDM">
+                        <div class="event" ></div>
+                    </div>
+                    <div class="cart mc " id="CM">
+                        <div class="event" ></div>
+                    </div>
+                    <div class="cart cam " id="CAM">
+                        <div class="event" ></div>
+                    </div>
+                </div>
+    
+                <div class="defense">
+                    <div class="cart lb " id="LB">
+                        <div class="event" ></div>
+                    </div>
+                    <div class="cart cb " id="CBR">
+                        <div class="event" ></div>
+                    </div>
+                    <div class="cart cb " id="CBL">
+                        <div class="event" ></div>
+                    </div>
+                    <div class="cart rb " id="RB">
+                        <div class="event" ></div>
+                    </div>
+                </div>
+    
+                <div class="goal ">
+                    <div class="cart gk " id="GK">
+                        <div class="event" ></div>
+                    </div>
+                </div>
+                
+            </div>
+
+            <div id="replacement" class="replacements">
+               
+            </div>
+
+        </div>
+</body>
+</html>
